648feef2fc273c2119c09167f6ffe6366bc06b76
Melanie Corelli <mcorelli10@MVCCLABS.EDU>
2017-01-31 13:44:39 -0500

    initial add

---
+++ DisplayNumbers.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Display Numbers</title>
+</head>
+
+<body>
+<?php
+$IntegerVar = 150;
+$FloatingPointVar = 3.0e7; //floating-point
+							//number 30,000,000
+echo "<p>Integer variable: $IntegerVar<br />";
+echo "Floating-point variable: $FloatingPointVar</p>";
+?>
+
+
+</body>
+</html>
