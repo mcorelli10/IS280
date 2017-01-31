bef9c2d486c3229bcf7f224dd92ce49db7cb6760
Melanie Corelli <mcorelli10@MVCCLABS.EDU>
2017-01-31 13:23:46 -0500

    initial add

---
+++ PirateTalk.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>"How to Talk Like a Pirate"</title>
+</head>
+
+<body>
+<h1>How to Talk Like a Pirate</h1>
+<p>
+<?php
+echo "<p>Avast me hearties!<br />", 
+"Return handsome with some fine swag,<br />
+ye scurvy dogs!<br />",
+"Else, we be keelhaulin' ye' next morn....</p>";
+?>
+</p>
+</body>
+</html>
